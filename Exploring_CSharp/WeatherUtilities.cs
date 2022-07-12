b43c9fad36ccf54ce38b6d357a3a804f34825b3f
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-12 16:28:30 +0100

    refactor shortcut in vs: Quick Actions & Refactorings -> Inline temporary variable

--- Exploring_CSharp/WeatherUtilities.cs
+++ Exploring_CSharp/WeatherUtilities.cs
@@ -6,14 +6,12 @@ namespace Exploring_CSharp
     {
         static float FahrenheitToCelsius(float temperatureFahrenheit)
         {
-            var temperatureCelsius = temperatureFahrenheit - 32 / 1.8f;
-            return temperatureCelsius;
+            return (float)(temperatureFahrenheit - 32 / 1.8f);
         }
 
         static float CelsiusToFahrenheit(float temperatureCelsius)
         {
-            var temperatureFahrenheit = temperatureCelsius * 1.8f + 32;
-            return temperatureFahrenheit;
+            return (float)(temperatureCelsius * 1.8f + 32);
         }
 
         // The higher the index, the lower the comfort
