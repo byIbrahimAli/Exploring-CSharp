bea977100123f9927c86689b5b4a0fcacd38459a
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-12 15:53:21 +0100

    learnt naming conventions & basic method syntax

---
+++ Exploring_CSharp/NamingConventionsAndBasicFunction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exploring_CSharp // Note: actual namespace depends on the project name.
+{
+    internal class NamingConventionsAndBasicFunction
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static float FahrenheitToCelsius(float temperatureFahrenheit)
+        {
+            float temperatureCelsius = (temperatureFahrenheit - 32) / 1.8f;
+            // all decimals "1.8" are double's and not floats because
+            // doubles are more precise. It's an 'unsafe conversion' from
+            // a double to a float, hence the error.
+
+            // to write a decimal as a float, add 'f' afterwards: "1.8f"
+            return temperatureCelsius;
+        }
+
+        /**
+         * method (function) names are uppercase camelback and don't typically 
+         * have _ to seperate words -> ThisIsAFunctionNameExample
+         * 
+         * methods are called similar to java, class.methodname(arguments);
+         * 
+         * argument names use lowercase camelback and again don't use _ to 
+         * seperate words -> thisIsAnArgument
+         * 
+         * code layout is different to java:
+         *  void javaMethod() {
+         *      System.out.println("yeee");
+         *  }
+         * in C# the there is no code on the same line as the curly braces:
+         *  void CSharpMethod()
+         *  {
+         *      Console.WriteLine("yeee");
+         *  }
+         * 
+         */
+    }
+}
