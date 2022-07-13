a5a156eddb2b1a01a9611051c6f59f8c76e06b5d
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-13 20:55:58 +0100

    used quick action to properly order public before static in declaration of methods

--- Exploring_CSharp/Weather/WeatherUtilities.cs
+++ Exploring_CSharp/Weather/WeatherUtilities.cs
@@ -4,24 +4,24 @@ namespace Pluralsight.MyUtilities
 {
     class WeatherUtilities
     {
-        static public float FahrenheitToCelsius(float temperatureFahrenheit)
+        public static float FahrenheitToCelsius(float temperatureFahrenheit)
         {
             return (float)((temperatureFahrenheit - 32) / 1.8f);
         }
 
-        static public float CelsiusToFahrenheit(float temperatureCelsius)
+        public static float CelsiusToFahrenheit(float temperatureCelsius)
         {
             return (float)(temperatureCelsius * 1.8f + 32);
         }
 
         // The higher the index, the lower the comfort
-        static private float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        private static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
         {
             // Probably not a very reliable formula          
             return (temperatureFahrenheit + humidityPercent) / 4;
         }
 
-        static public void Report(string location, float temperatureCelsius, float humidity)
+        public static void Report(string location, float temperatureCelsius, float humidity)
         {
             var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
             Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
