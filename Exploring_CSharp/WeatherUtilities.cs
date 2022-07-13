c6676db13afc30efd687638fb6fdde906b5a3ff9
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-13 18:26:36 +0100

    methods that aren’t explicitly public or private are by default private.

--- Exploring_CSharp/WeatherUtilities.cs
+++ Exploring_CSharp/WeatherUtilities.cs
@@ -1,27 +1,27 @@
 using System;
 
-namespace Exploring_CSharp
+namespace Pluralsight.MyUtilities
 {
     class WeatherUtilities
     {
-        static float FahrenheitToCelsius(float temperatureFahrenheit)
+        static public float FahrenheitToCelsius(float temperatureFahrenheit)
         {
             return (float)(temperatureFahrenheit - 32 / 1.8f);
         }
 
-        static float CelsiusToFahrenheit(float temperatureCelsius)
+        static public float CelsiusToFahrenheit(float temperatureCelsius)
         {
             return (float)(temperatureCelsius * 1.8f + 32);
         }
 
         // The higher the index, the lower the comfort
-        static float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        static public float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
         {
             // Probably not a very reliable formula          
             return (temperatureFahrenheit + humidityPercent) / 4;
         }
 
-        static void Report(string location, float temperatureCelsius, float humidity)
+        static public void Report(string location, float temperatureCelsius, float humidity)
         {
             var temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
             Console.WriteLine($"Comfort Index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
