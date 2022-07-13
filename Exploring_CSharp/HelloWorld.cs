7034c87d28473de34e3385e7750dced99467c609
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-13 17:08:37 +0100

    basic hello world, woah, amazing

---
+++ Exploring_CSharp/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Exploring_CSharp
+{
+    public class HelloWorld
+    {
+        public HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
