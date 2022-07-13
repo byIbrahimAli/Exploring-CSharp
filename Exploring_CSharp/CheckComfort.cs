e14fab6b89069a5d80d37afc2986bda27619894b
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-13 17:32:48 +0100

    converting helloworld to a class which uses WeatherUtilities. Namespaces have changed.

---
+++ Exploring_CSharp/CheckComfort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exploring_CSharp
+{
+    public class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            WeatherUtilities.Report("Bologna", 23, 65); // ~73 F
+        }
+    }
+}
+
