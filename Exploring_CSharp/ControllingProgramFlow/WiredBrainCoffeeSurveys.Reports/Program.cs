7c6af3f6b990a381894112d2f0af868ec9626616
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-15 15:51:21 +0100

    finding response rate and how many of 500 participants didn’t answer survey.

--- Exploring_CSharp/ControllingProgramFlow/WiredBrainCoffeeSurveys.Reports/Program.cs
+++ Exploring_CSharp/ControllingProgramFlow/WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -6,23 +6,31 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
-            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
-            Console.WriteLine($"Response Percentage: {responseRate}");
+            Console.WriteLine($"Response Percentage: {responseRate}"); // needs $ prefix to print var in string
             Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
-            Console.WriteLine($"Overall Score: {overallScore}");
-
-            // Logical comparisons
-            bool isCoffeeScoreLower = Q1Results.CoffeeScore < Q1Results.FoodScore;
-            bool customersRecommend = Q1Results.WouldRecommend >= 7;
-            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
-
-            Console.WriteLine($"Coffee Score Higher Than Food: {isCoffeeScoreLower}");
-            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
-            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
         }
     }
 }
+
+// Don't know what they wanted me to do with this crap:
+
+//// Calculated Values
+//double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+//double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+//double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+//Console.WriteLine($"Response Percentage: {responseRate}");
+//Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+//Console.WriteLine($"Overall Score: {overallScore}");
+
+//// Logical comparisons
+//bool isCoffeeScoreLower = Q1Results.CoffeeScore < Q1Results.FoodScore;
+//bool customersRecommend = Q1Results.WouldRecommend >= 7;
+//bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+
+//Console.WriteLine($"Coffee Score Higher Than Food: {isCoffeeScoreLower}");
+//Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
+//Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
