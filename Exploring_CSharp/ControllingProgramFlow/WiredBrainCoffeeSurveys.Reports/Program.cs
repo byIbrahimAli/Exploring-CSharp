1b486ae88ef5f34b2e936d29f9c6f05846c2905f
Ibrahim <59145840+byIbrahimAli@users.noreply.github.com>
2022-07-15 17:10:42 +0100

    this guy is boring

--- Exploring_CSharp/ControllingProgramFlow/WiredBrainCoffeeSurveys.Reports/Program.cs
+++ Exploring_CSharp/ControllingProgramFlow/WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -6,11 +6,25 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
             Console.WriteLine($"Response Percentage: {responseRate}"); // needs $ prefix to print var in string
             Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+            Console.WriteLine($"Overall score: {overallScore}");
+
+            // Logical comparisons
+            bool isCoffeeScoreLower = Q1Results.CoffeeScore > Q1Results.FoodScore;
+            bool customersRecommend = Q1Results.WouldRecommend >= 7;
+            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino"; // performs comparison == and not assignment =
+
+            Console.WriteLine($"Coffee Score Higher Than Food: {isCoffeeScoreLower}");
+            Console.WriteLine($"Customers Would Recommend Us: {customersRecommend}");
+            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
+
+
         }
     }
 }
